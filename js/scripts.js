5faaa78e9a4b9961fc51bc136355a573346ad270
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-12 16:22:51 -0700

    Add UI for subtraction. and code for html for subtraction.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,15 @@ $(document).ready(function() {
 		event.preventDefault();
 		const number1 = parseInt ($("#add1").val());
 		const number2 = parseInt ($("#add2").val());
-	
-		alert(add(number1, number2));
+		const result = add(number1, number2);
+		$("#output1").text(result);
 	});
-});	
+
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt ($("#subtract1").val());
+		const number2 = parseInt ($("#subtract2").val());
+		const result = subtract(number1, number2);
+		$("#output2").text(result);
+	});
+});
