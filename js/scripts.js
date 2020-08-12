ee8a45c6817945cafc6a2b883a0d95c43dc66d45
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-12 15:09:09 -0700

    Add punctuation in JS.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,28 @@
 
 	const add = function(number1, number2) {
 		return number1 + number2;
-	};
+	}
 
 	const multiply = function(number1, number2) {
 		return number1 * number2;
-	};
+	}
 
 	const divide = function(number1, number2) {
 		return number1 / number2; 
-	};
+	}
 
 	const subtract = function(number1, number2) {
 		return number1 - number2;
-	};
+	}
 
 	// Everything below this line is user interface logic:
 
-	$(document).ready(function() {
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
 		const number1 = parseInt ($("#add1").val());
 		const number2 = parseInt ($("#add2").val());
 	
 		alert(add(number1, number2));
+	});
 });	
